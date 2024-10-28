1482b39f34b3a62ff5bd0af42d68fd7862c96c7c
elijahsandler <sandler.e@northeastern.edu>
2024-10-28 12:54:52 -0400

    library housekeeping

--- library/src/epigraph-elijahsandler/templates/__init__.py
+++ library/src/epigraph-elijahsandler/templates/__init__.py
@@ -1,12 +1,2 @@
-import json
-import os
-import plotly.io as pio
-
-# Define the path to the netsi template JSON file
-template_path = os.path.join(os.path.dirname(__file__), 'netsi.json')
-with open(template_path, "r") as f:
-    netsi_template = json.load(f)
-
-# Register the template and expose it as an attribute
-pio.templates["netsi"] = netsi_template
-netsi = "netsi"
+# allows `from epigraph.templates import netsi`
+from .netsi import netsi 
